7e5a1b41bb69ffdc13a79681920873d133fb8a98
Dawid Piechaczek <dawidpiechaczek.20@gmail.com>
2022-02-24 22:14:49 +0100

    Splash screen implementation for api 31.

--- app/src/main/java/com/appsirise/trans4fans/main/MainActivity.kt
+++ app/src/main/java/com/appsirise/trans4fans/main/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.appsirise.trans4fans.main
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.appsirise.core.ui.base.BaseActivity
 import com.appsirise.trans4fans.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
 
     override fun setBinding() = ActivityMainBinding.inflate(layoutInflater)
 }
