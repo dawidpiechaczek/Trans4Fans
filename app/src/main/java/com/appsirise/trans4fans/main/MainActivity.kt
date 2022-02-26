094a006fbe1207d946627ab5b2e5f9b7a4cc34d8
Dawid Piechaczek <dawidpiechaczek.20@gmail.com>
2022-02-26 10:41:48 +0100

    Add theme change for api lower than 31 in activity after splash.

--- app/src/main/java/com/appsirise/trans4fans/main/MainActivity.kt
+++ app/src/main/java/com/appsirise/trans4fans/main/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.appsirise.trans4fans.main
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.appsirise.core.ui.base.BaseActivity
+import com.appsirise.trans4fans.R
 import com.appsirise.trans4fans.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Trans4Fans)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
-    override fun setBinding() = ActivityMainBinding.inflate(layoutInflater)
 }
